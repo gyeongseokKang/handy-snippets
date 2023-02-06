f870f5b2912254be1f850ad682a5f83aa0a80a1c
handy <hk@gaudiolab.com>
2023-02-06 18:59:39 +0900

    [Feature] 0.0.3

---
+++ example/edcp.tsx
@@ -0,0 +1,17 @@
+interface edcpProps {}
+
+const edcp = ({}: edcpProps) => {
+  return <></>;
+};
+
+export default edcp;
+
+/*
+interface ${1:${TM_FILENAME_BASE}}Props {}
+
+const ${1:${TM_FILENAME_BASE}} = ({}: ${1:${TM_FILENAME_BASE}}Props) => {
+  return <>$2</>;
+};
+
+export default ${1:${TM_FILENAME_BASE}};
+ */
